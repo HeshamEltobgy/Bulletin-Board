52034f2116cfa686486fc40db7850d3c3eafaf7c
Hesham Eltobgy <hesham.eltobgy@griffithuni.edu.au>
2019-01-27 22:02:23 +0800

    fix issue with deployment

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.557bc424b448cfb0c50aa8dea0870737.js"
+  "/Bulletin-Board/precache-manifest.91a2345079b98ccbf873ae7cc83bf145.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/Bulletin-Board/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
