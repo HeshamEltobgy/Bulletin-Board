9acb662fd03a5b4ecb1cb9b222f527cb41875cff
Hesham Eltobgy <hesham.eltobgy@griffithuni.edu.au>
2019-01-27 12:09:56 +0800

    create react-2-project

---
+++ build/precache-manifest.78db9525b50091dae5a87bff4fabc394.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c4c64e1d3dc17d4d8315",
+    "url": "/static/js/main.c4c64e1d.chunk.js"
+  },
+  {
+    "revision": "640cbbf3a5232ddc938d",
+    "url": "/static/js/1.640cbbf3.chunk.js"
+  },
+  {
+    "revision": "c4c64e1d3dc17d4d8315",
+    "url": "/static/css/main.a734b5d1.chunk.css"
+  },
+  {
+    "revision": "d9bffc385332baef75e1cbbd6feee355",
+    "url": "/index.html"
+  }
+];
