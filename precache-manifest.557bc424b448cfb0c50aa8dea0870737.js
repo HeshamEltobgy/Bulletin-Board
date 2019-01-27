8e1263afc66a61d891dc5880caf8258daf98148f
Hesham Eltobgy <hesham.eltobgy@griffithuni.edu.au>
2019-01-27 21:32:59 +0800

    Updates

---
+++ precache-manifest.557bc424b448cfb0c50aa8dea0870737.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9b000f53720eb82b6e5b",
+    "url": "/static/js/main.9b000f53.chunk.js"
+  },
+  {
+    "revision": "640cbbf3a5232ddc938d",
+    "url": "/static/js/1.640cbbf3.chunk.js"
+  },
+  {
+    "revision": "9b000f53720eb82b6e5b",
+    "url": "/static/css/main.d87d1f11.chunk.css"
+  },
+  {
+    "revision": "9ebb8e77c4ff9adeb41e9fd8d96d0af5",
+    "url": "/index.html"
+  }
+];
