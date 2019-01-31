f5670f648e14f9fefde13b9ecdd95ff4ce20b931
Hesham Eltobgy <hesham.eltobgy@griffithuni.edu.au>
2019-01-31 12:06:09 +0800

    update github page

--- build/precache-manifest.d396e0d90b51e25e0587ac18c658442d.js
+++ build/precache-manifest.d396e0d90b51e25e0587ac18c658442d.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/Bulletin-Board/static/js/runtime~main.e18a1111.js"
   },
   {
-    "revision": "3e9f85eb26c41212aaab",
-    "url": "/Bulletin-Board/static/js/main.3e9f85eb.chunk.js"
+    "revision": "5ddef059f0f680058ed4",
+    "url": "/Bulletin-Board/static/js/main.5ddef059.chunk.js"
   },
   {
     "revision": "640cbbf3a5232ddc938d",
     "url": "/Bulletin-Board/static/js/1.640cbbf3.chunk.js"
   },
   {
-    "revision": "3e9f85eb26c41212aaab",
-    "url": "/Bulletin-Board/static/css/main.d87d1f11.chunk.css"
+    "revision": "5ddef059f0f680058ed4",
+    "url": "/Bulletin-Board/static/css/main.542117e5.chunk.css"
   },
   {
-    "revision": "79e0c97be8bf66d92d56ae3395fcc3ac",
+    "revision": "b143ace6bb17aa13baa39f5ae07056ca",
     "url": "/Bulletin-Board/index.html"
   }
 ];
